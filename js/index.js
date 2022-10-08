00cda52e3891c548c5f448291cbccac3ba918f92
Saad-Surve <95091209+Saad-Surve@users.noreply.github.com>
2022-10-08 16:03:32 +0530

    upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const myPreloader = document.querySelector('.preloader');
+const fadeOutEffect = setInterval(() => {
+    if (!preloader.style.opacity) {
+      preloader.style.opacity = 1;
+    }
+    if (preloader.style.opacity > 0) {
+      preloader.style.opacity -= 0.5;
+    } else {
+      clearInterval(fadeEffect);
+    }
+  }, 300);
+  window.addEventListener('load', fadeOutEffect);
