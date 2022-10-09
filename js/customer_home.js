299bd835cb8613a70ea558c2f2e14b15a328a461
Saad-Surve <95091209+Saad-Surve@users.noreply.github.com>
2022-10-09 15:22:17 +0530

    added page after login

---
+++ js/customer_home.js
@@ -0,0 +1,35 @@
+function profile() {
+    document.getElementById('profile').style.display="inline";
+    document.getElementById('account').style.display="none";
+    document.getElementById('transactions').style.display="none";
+    document.getElementById('services').style.display="none";
+    document.getElementById('contact').style.display="none";
+}
+function account() {
+    document.getElementById('profile').style.display="none";
+    document.getElementById('account').style.display="inline";
+    document.getElementById('transactions').style.display="none";
+    document.getElementById('services').style.display="none";
+    document.getElementById('contact').style.display="none";
+}
+function transactions() {
+    document.getElementById('profile').style.display="none";
+    document.getElementById('account').style.display="none";
+    document.getElementById('transactions').style.display="inline";
+    document.getElementById('services').style.display="none";
+    document.getElementById('contact').style.display="none";
+}
+function services() {
+    document.getElementById('profile').style.display="none";
+    document.getElementById('account').style.display="none";
+    document.getElementById('transactions').style.display="none";
+    document.getElementById('services').style.display="inline";
+    document.getElementById('contact').style.display="none";
+}
+function contact() {
+    document.getElementById('profile').style.display="none";
+    document.getElementById('account').style.display="none";
+    document.getElementById('transactions').style.display="none";
+    document.getElementById('services').style.display="none";
+    document.getElementById('contact').style.display="inline";
+}
