f4293bd8daeb473d15919cc589efb78d26da1846
mustalidhanerawala <dhanerawala5@hotmail.com>
2024-12-31 18:04:18 +0530

    upgrade4

--- js/buycurrencies.js
+++ js/buycurrencies.js
@@ -1,12 +1,37 @@
 console.log("Hello World!")
-const URL = "https://api.exchangerate-api.com/v4/latest/USD";
-//Bro idhar api url ko store kar raha hu
-async function catch1() 
+const URL1 = "https://api.exchangerate-api.com/v4/latest/USD";
+const URL2 = "https://restcountries.com/v3.1/alpha/IN";
+
+
+let from = document.getElementById("from");
+let to = document.getElementById("to");
+let flag1 = document.getElementById("flag1");
+let flag2 = document.getElementById("flag2");
+console.log(flag1.src)
+
+async function changeflag1(element) 
 {
-    let response = await fetch(URL)
-    console.log(response)
+    console.log(element.value);
+    console.log(element)
+    const URL3 = `https://restcountries.com/v3.1/alpha/${element.value}`;
+    let response = await fetch(URL3)
     let data = await response.json()
-    console.log(data)
+    let flaglink = data[0].flags.png
+    flag1.src = flaglink;
+
+
+}
+async function changeflag2(element) {
+    console.log(element.value);
+    console.log(element)
+    const URL3 = `https://restcountries.com/v3.1/alpha/${element.value}`;
+    let response = await fetch(URL3)
+    let data = await response.json()
+    let flaglink = data[0].flags.png
+    flag2.src = flaglink;
+
 }
 
-catch1()
+
+from.addEventListener("change", (e) => changeflag1(e.target));
+to.addEventListener("change", (e) => changeflag2(e.target));
